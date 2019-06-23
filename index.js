3bc2e90da0094c8eb2b5619c650bc7780390284a
maschad <chad.nehemiah94@gmail.com>
2019-06-23 00:52:36 -0500

    shimmed to RN

--- index.js
+++ index.js
@@ -6,4 +6,8 @@ import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
 
+// Import shim
+import "./shims"
+
+
 AppRegistry.registerComponent(appName, () => App);
