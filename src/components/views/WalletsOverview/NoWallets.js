1ce4dddb8017841bff691499bacc437238f709bc
maschad <chad.nehemiah94@gmail.com>
2019-11-12 23:58:02 -0500

    added vector icons and fab

--- src/components/views/WalletsOverview/NoWallets.js
+++ src/components/views/WalletsOverview/NoWallets.js
@@ -1,24 +1,49 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import {StyleSheet, View} from 'react-native';
-import { Content, Card, CardItem, Text, Body } from 'native-base';
+import { Body, Button, Content, Card, CardItem, Fab, Text } from 'native-base';
+import Icon from 'react-native-vector-icons/FontAwesome';
 
+ 
+export default () => {
+    const [active, setActive] = useState(false);
 
-export default () => (
-     <View style={styles.container}>
-        <Content>
-            <Card>
-                <CardItem header>
-                    <Text>No Wallets</Text>
-                </CardItem>
-                <CardItem>
-                    <Body>
-                        <Text>Create a wallet</Text>
-                    </Body>
-                </CardItem>
-            </Card>
-        </Content>
-     </View>
-);
+    const onPress = () => {
+        setActive(!active);
+    }
+
+    return (
+        <View style={styles.container}>
+            <Content>
+                <Card>
+                    <CardItem header>
+                        <Text>Oops! Looks like you have no Wallets :(</Text>
+                    </CardItem>
+                    <CardItem>
+                        <Body>
+                            <Text>Click the button below to add one :)</Text>
+                        </Body>
+                    </CardItem>
+                </Card>
+            </Content>
+            <Fab
+                active={active}
+                direction="up"
+                containerStyle={{ }}
+                style={{ backgroundColor: '#000' }}
+                position="bottomRight"
+                onPress={() => onPress()}>
+                <Icon name="plus" size={30} color="#000" />
+                <Button style={{ backgroundColor: '#FFF' }}>
+                    <Icon name="bitcoin" size={30} color="#000" />
+                </Button>
+                <Button style={{ backgroundColor: '#3B5998' }}>
+                    <Icon name="ethereum" size={30} color="#900" />
+                </Button>
+          </Fab>
+        </View>
+    );
+}
+ 
 
 const styles = StyleSheet.create({
     container: {
